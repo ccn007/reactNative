49539220c87a3dd687387180d220a885c5ad6a0c
ccn007 <ni.chaichana_st@tni.ac.th>
2023-08-26 11:09:04 +0700

    First Commit

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import FlatList_HeaderFooter from './components/FlatList_HeaderFooter'
-
+import { StyleSheet,View, Text } from "react-native";
+import React from "react";
+import TouchableExample from "./components/TouchableExample";
+import RecapTouchable from "./components/RecapTouchable";
+import Example_UseEffect from "./components/Example_UseEffect";
+import UseEffectFlatList from "./components/UseEffectFlatList";
+import RandomUsersScreen from "./components/RandomUsersScreen";
+import FlatList_Example1 from "./components/FlatList_Example1";
+import FlatList_HeaderFooter from "./components/FlatList_HeaderFooter";
+import FlatListAPI from "./components/FlatListAPI";
+import News from "./components/News";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-        <FlatList_HeaderFooter/>
+    <View style = {styles.container}>
+
+      <FlatList_HeaderFooter/>
+      <News/>
     </View>
-  )
-}
-
-export default App
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,   
-    justifyContent:'center',    
-    alignItems:'left'   
- }
-})
+  );
+};
